0e4f0aa3904b68fdef971514d0621b8d99be6554
Gargonium <tetatet.nikvot@gmail.com>
2026-05-25 16:54:56 +0700

    finale init

--- Library/init.sql
+++ Library/init.sql
@@ -5,14 +5,28 @@ create table libraries
     address varchar(255) not null
 );
 
-create type hall_type_T as enum ('abonnement', 'reading_room');
-
-create table halls
+create table reading_rooms
 (
     id         serial primary key,
-    library_id int          not null references libraries (id) on delete cascade,
-    hall_type  hall_type_T  not null,
-    name       varchar(255) not null
+    library_id int not null references libraries (id) on delete restrict
+);
+
+create table storages
+(
+    id         serial primary key,
+    library_id int not null references libraries (id) on delete restrict
+);
+
+create table racks
+(
+    id         serial primary key,
+    storage_id int not null references storages (id) on delete restrict
+);
+
+create table shelves
+(
+    id      serial primary key,
+    rack_id int not null references racks (id) on delete restrict
 );
 
 create table reader_categories
@@ -24,7 +38,8 @@ create table reader_categories
 create table readers
 (
     id                serial primary key,
-    category_id       int          not null references reader_categories (id) on delete restrict,
+    category_id       int          not null references reader_categories (id),
+    unique (id, category_id),
     library_id        int          not null references libraries (id) on delete restrict,
     first_name        varchar(255) not null,
     last_name         varchar(255) not null,
@@ -35,15 +50,19 @@ create table reader_category_attributes
 (
     id          serial primary key,
     category_id int          not null references reader_categories (id) on delete cascade,
+    unique (id, category_id),
     name        varchar(255) not null
 );
 
 create table reader_category_attribute_values
 (
-    reader_id    int          not null references readers (id) on delete cascade,
-    attribute_id int          not null references reader_category_attributes (id) on delete cascade,
+    reader_id    int          not null,
+    category_id  int          not null,
+    attribute_id int          not null,
     value        varchar(255) not null,
-    primary key (reader_id, attribute_id)
+    primary key (reader_id, attribute_id),
+    foreign key (reader_id, category_id) references readers (id, category_id),
+    foreign key (attribute_id, category_id) references reader_category_attributes (id, category_id)
 );
 
 create table work_categories
@@ -52,31 +71,47 @@ create table work_categories
     name varchar(255) not null unique
 );
 
+create table authors
+(
+    id         serial primary key,
+    first_name varchar(255),
+    last_name  varchar(255) not null
+);
+
 create table works
 (
     id          serial primary key,
     title       varchar(255) not null,
-    category_id int          not null references work_categories (id) on delete restrict
+    category_id int          not null references work_categories (id) on delete restrict,
+    unique (id, category_id),
+    author      int          not null references authors (id) on delete restrict
 );
 
 create table work_category_attributes
 (
     id          serial primary key,
     category_id int          not null references work_categories (id) on delete cascade,
+    unique (id, category_id),
     name        varchar(255) not null
 );
 
 create table work_category_attribute_values
 (
-    work_id      int          not null references works (id) on delete cascade,
-    attribute_id int          not null references work_category_attributes (id) on delete cascade,
+    work_id      int          not null,
+    category_id  int          not null,
+    attribute_id int          not null,
     value        varchar(255) not null,
-    primary key (work_id, attribute_id)
+    primary key (work_id, attribute_id),
+    foreign key (work_id, category_id) references works (id, category_id),
+    foreign key (attribute_id, category_id) references work_category_attributes (id, category_id)
 );
 
 create table publications
 (
-    id serial primary key
+    id           serial primary key,
+    title        varchar(255) not null,
+    publish_date date         not null,
+    publisher    varchar(255) not null
 );
 
 create table publications_works
@@ -94,28 +129,54 @@ create table publication_rules
     check ((reading_room_only = true and loan_period_days is null) or (reading_room_only = false))
 );
 
-create type publications_status_T as enum ('available', 'loaned');
+create table employee
+(
+    id              serial primary key,
+    library_id      int          not null references libraries (id) on delete restrict,
+    reading_room_id int          not null references reading_rooms (id) on delete restrict,
+    first_name      varchar(255) not null,
+    last_name       varchar(255) not null
+);
 
 create table publications_copy
 (
-    id                  serial primary key,
-    inventory_number    varchar(255)          not null unique,
-    publication_id      int                   not null references publications (id) on delete restrict,
-    publications_status publications_status_T not null default 'available',
-    library_id          int                   not null references libraries (id) on delete restrict
+    inventory_number     int  not null unique primary key,
+    publication_id       int  not null references publications (id) on delete restrict,
+    shelf_id             int  not null references shelves (id) on delete restrict,
+    receipt_date         date not null,
+    received_employee_id int  not null references employee (id) on delete restrict
+);
+
+create table written_off
+(
+    id                    serial primary key,
+    inventory_number      varchar(255) not null unique,
+    publication_id        int          not null references publications (id) on delete restrict,
+    write_off_date        date         not null,
+    write_off_employee_id int          not null references employee (id)
 );
 
 create table loans
 (
-    id        serial primary key,
-    reader_id int not null references readers (id) on delete restrict,
-    copy_id   int not null references publications_copy (id) on delete restrict
+    id                    serial primary key,
+    reader_id             int  not null references readers (id) on delete restrict,
+    copy_inventory_number int  not null references publications_copy (inventory_number) on delete restrict,
+    date_of_issue         date not null,
+    return_date           date check ( return_date > loans.date_of_issue ),
+    expire_date           date not null check ( expire_date > loans.date_of_issue ),
+    issued_employee_id    int  not null references employee (id) on delete restrict
 );
 
-create table employee
+create table reading_process
 (
-    id         serial primary key,
-    library_id int          not null references libraries (id) on delete restrict,
-    first_name varchar(255) not null,
-    last_name  varchar(255) not null
+    id                    serial primary key,
+    reader_id             int  not null references readers (id) on delete cascade,
+    copy_inventory_number int  not null references publications_copy (inventory_number) on delete cascade,
+    issued_employee_id    int  not null references employee (id) on delete cascade,
+    library_id            int  not null references libraries (id) on delete cascade,
+    reading_room_id       int  not null references reading_rooms (id) on delete cascade,
+    issued_date           date not null default current_date,
+    issued_time           time not null,
+    return_date           date,
+    return_time           time check ( return_time > reading_process.issued_time )
 );
